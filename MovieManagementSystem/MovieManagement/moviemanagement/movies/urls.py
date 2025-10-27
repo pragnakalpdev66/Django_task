8f250a01493c3b062d9cdfe2d71fb47802ab525e
pragnakalpdev66 <pragnakalp.dev66@gmail.com>
2025-10-27 22:12:53 +0530

    update code with improvised

--- MovieManagementSystem/MovieManagement/moviemanagement/movies/urls.py
+++ MovieManagementSystem/MovieManagement/moviemanagement/movies/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 
     path('moviepage/', views.MoviePageView.as_view(), name='movie'),
     path('addmoviepage/', views.AddMovieView.as_view(), name='addmovie'),
-    path('movie/manage_cast_languages/<int:movie_id>/', views.ManageCastLanguagesView.as_view(), name='manage-cast-languages'),
+    path('movie/manage_cast_language/<int:movie_id>/', views.ManageCastLanguagesView.as_view(), name='manage_cast_language'),
     path('movieDetailpage/', views.MovieDetailView.as_view(), name='movieDetail'),
     
     path('genrepage/', views.GenrePageView.as_view(), name='genre'),
@@ -16,4 +16,5 @@ urlpatterns = [
     
     path('peoplepage/', views.PeoplePageView.as_view(), name='people'),
     path('addpeoplepage/', views.AddPeopleView.as_view(), name='addpeople'),
+    path('personDetailpage/', views.PersonDetailView.as_view(), name='personDetail'),
 ]
